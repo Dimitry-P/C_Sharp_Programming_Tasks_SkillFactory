d4350202e1eb832d845ae7bf9f15692e149b7ca6
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-04 10:10:15 +0300

    Task3: Output to the console a filled circle in the form of stars by specifying the coordinates of the center of the circle and its radius

---
+++ Task3_Coordinates_of_the_center_of_the_circle_and_its_radius.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите координаты центра круга и его радиус:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int radius = int.Parse(Console.ReadLine());
+
+        for (int i = x - radius; i <= x + radius; i++)
+        {
+            for (int l = y - radius; l <= y + radius; l++)
+            {
+                if ((i - x) * (i - x) + (l - y) * (l - y) <= radius * radius)
+                {
+                    Console.SetCursorPosition(i, l);
+                    Console.WriteLine("*");
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
