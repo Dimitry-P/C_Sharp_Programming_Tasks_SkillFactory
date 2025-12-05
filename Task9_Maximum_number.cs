41ad2de763a1ea71d98e9cb432bf6084ceb17b12
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-05 09:37:11 +0300

    Task9: Maximum number (Program)

---
+++ Task9_Maximum_number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int max;
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int e = int.Parse(Console.ReadLine());
+
+        max = a;
+
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+        if (e > max) max = e;
+
+        Console.WriteLine("Максимальное число: " + max);
+        Console.ReadKey();
+    }
+}
