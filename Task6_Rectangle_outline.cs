90bc6ca0a0d37f581a86c32ae447ed59491e3e50
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-05 09:26:46 +0300

    Task6: Draw a rectangle by entering the coordinates of the top left and bottom right points

---
+++ Task6_Rectangle_outline.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите координаты левой верхней и нижней правой точки прямоугольника:");
+        int x1 = int.Parse(Console.ReadLine());
+        int y1 = int.Parse(Console.ReadLine());
+        int x2 = int.Parse(Console.ReadLine());
+        int y2 = int.Parse(Console.ReadLine());
+
+        for (int x = x1; x <= x2; x++)
+        {
+            for (int y = y1; y <= y2; y++)
+            {
+                if (x == x1 || x == x2 || y == y1 || y == y2)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("*");
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
