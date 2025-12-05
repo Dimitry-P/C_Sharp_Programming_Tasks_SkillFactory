939603be14b116ee5674b4dd971ceaac658535c6
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-05 09:34:23 +0300

    Task8: Finding the maximum number among the entered numbers (Program)

---
+++ Task8_Finding_the_maximum_number_among_the_entered_numbers .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int res2 = int.MinValue;
+        int result;
+        int amount = int.Parse(Console.ReadLine());
+
+        for (int i = amount; i > 0; i--)
+        {
+            result = int.Parse(Console.ReadLine());
+
+            if (res2 < result)
+            {
+                res2 = result;
+            }
+        }
+        Console.WriteLine($"Из чисел которые вы ввели максимальное {res2}");
+        Console.ReadLine();
+    }
+}
