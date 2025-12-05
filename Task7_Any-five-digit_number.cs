fd763bba1f5b8b20291d19b24aa9c153c1fedf06
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-05 09:29:47 +0300

    Task7: Any five-digit number input (Program)

---
+++ Task7_Any-five-digit_number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите любое пятизначное число: ");
+        string x = Console.ReadLine();
+        if (x.Length == 5)
+        {
+            Console.WriteLine(x[0]);
+            Console.WriteLine(x[1]);
+            Console.WriteLine(x[2]);
+            Console.WriteLine(x[3]);
+            Console.WriteLine(x[4]);
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное число");
+        }
+        Console.ReadKey();
+    }
+}
