efba483c4ae1a55e5fab04cc82c8bbc4e03016ed
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-07 21:00:10 +0300

    Task12: Program that finds the minimum and maximum number in an array

---
+++ Task12_Program_that_finds_the_minimum_and_maximum_number_in_an_array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Напишите программу, которая находит минимальное и максимальное число в массиве.
+        int[] D = new int[10] { 3, 2, 5, 1, 24, 6, 2, 8, 2, 9 };
+        for (int i = 0; i < D.Length; i++)
+        {
+            Console.Write(D[i] + " ");
+        }
+        Console.WriteLine();
+        int biggest = 0;
+        int smallest = 0;
+        for (int i = 0; i < D.Length; i++)
+        {
+            if (biggest < D[i]) biggest = D[i];
+
+        }
+        Console.WriteLine(biggest);
+        for (int i = 0; i < D.Length; i++)
+        {
+            smallest = D[i];
+            if (biggest > smallest) biggest = smallest;
+        }
+        Console.WriteLine(biggest);
+
+
+        int[] J = new int[10] { 3, 2, 5, 1, 24, 6, 2, 8, 2, 9 };
+        int min = J[0];
+        int max = J[0];
+        for (int i = 0; i < J.Length; i++)
+        {
+
+            if (J[i] < min) min = J[i];
+            if (J[i] > max) max = J[i];
+        }
+        Console.WriteLine(min);
+        Console.WriteLine(max);
+    }
+}
