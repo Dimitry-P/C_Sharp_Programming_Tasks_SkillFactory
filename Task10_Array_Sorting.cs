8a7b0286c37c39766ad0427a62af9fa0df8e51cb
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-07 20:46:30 +0300

    Task10: Array Sorting (Program)

---
+++ Task10_Array_Sorting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] Array = new int[5] { 5, 1, 2, 6, 4 };
+
+        for (int i = 0; i < Array.Length - 1; i++)
+        {
+            int t = Array[i];
+
+            if (Array[i] > Array[i + 1])
+            {
+                Array[i] = Array[i + 1];
+                Array[i + 1] = t;
+            }
+            for (int j = Array.Length - 1; j > 0; j--)
+            {
+                int tt = Array[j];
+                if (Array[j] < Array[j - 1])
+                {
+                    Array[j] = Array[j - 1];
+                    Array[j - 1] = tt;
+                }
+            }
+        }
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Console.Write(Array[i] + " ");
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
