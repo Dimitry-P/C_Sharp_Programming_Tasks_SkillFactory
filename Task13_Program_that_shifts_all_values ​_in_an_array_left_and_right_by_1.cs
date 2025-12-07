26795495f80fae881513e2d408d74fe9286a9850
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-07 21:03:28 +0300

    Task13: Program that shifts all values ​​in an array left and right by 1

---
+++ Task13_Program_that_shifts_all_values ​_in_an_array_left_and_right_by_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // все значения массива влево на 1
+        int[] L = new int[10] { 34, 16, 85, 8, 65, 60, 16, 32, 21, 83 };
+        for (int i = 0; i < L.Length; i++)
+        {
+            Console.Write(L[i] + " ");
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i < L.Length; i++)
+        {
+            Console.Write(L[i] + " ");
+        }
+        Console.Write(L[L.Length - 1] = L[0]);
+
+        //все значения массива вправо на 1
+        Console.WriteLine();
+
+        int[] Array = new int[10] { 65, 51, 57, 40, 25, 75, 10, 55, 73, 67 };
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Console.Write(Array[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.Write(Array[Array.Length - 1] + " ");
+        for (int i = 0; i < Array.Length - 1; i++)
+        {
+            Console.Write(Array[i] + " ");
+        }
+    }
+}
