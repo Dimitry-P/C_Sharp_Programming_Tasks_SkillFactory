42216bfaad4788b05ece413e7bba9be4602aa92c
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-09 10:52:45 +0300

    Task16: Perimeter and area of ​​a rectangle (Program)

---
+++ Task16_Perimeter_and_area_of_a_rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ширину прямоугольника: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите высоту прямоугольника: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Площадь прямоугольника равна: " + x * y);
+        Console.WriteLine("Периметр прямоугольника равен: " + 2 * (x + y));
+        Console.ReadLine();
+    }
+}
