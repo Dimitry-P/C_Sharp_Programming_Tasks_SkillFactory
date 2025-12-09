f98d6fe8abfc42dcab59eadb8aa1ec6f75cd90b0
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-09 10:37:07 +0300

    Task14: Greatest common divisor (Program)

---
+++ Task14_Greatest_common_divisor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int z = 0;
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        while (y > x)
+        {
+            int p = y - x;
+            z = x - p;
+            if (z == p) break;
+        }
+        while (y < x)
+        {
+            int p = x - y;
+            z = y - p;
+            if (z == p) break;
+
+        }
+
+        Console.WriteLine("НОД чисел " + x + " и " + y + " равен " + z);
+        Console.ReadLine();
+    }
+}
