c0072164790545bc08cad1ba95a59fd155e8014f
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-10 22:59:29 +0300

    Task17: Area of ​​a rectangle (Program)

---
+++ Task17_Area_of_a_rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите высоту и ширину прямоугольника");
+        int height = int.Parse(Console.ReadLine());
+        int width = int.Parse(Console.ReadLine());
+        int area = width * height;
+
+        Console.WriteLine("Площадь равна: " + area);
+
+        Console.ReadLine();
+    }
+}
