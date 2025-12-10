3edb166c3c1c26ec55a11b4454449e5f38887c3b
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-10 23:10:45 +0300

    Task20: String password (Program)

---
+++ Task20_String_password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string password = "Unity2020";
+        string bob = "";
+
+        while (bob != password)
+        {
+            Console.WriteLine("введите пароль");
+            bob = Console.ReadLine();
+        }
+
+        Console.WriteLine("вы вошли в систему");
+        Console.ReadLine();
+    }
+}
