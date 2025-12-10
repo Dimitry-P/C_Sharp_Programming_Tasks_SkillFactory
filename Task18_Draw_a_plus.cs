d0bf265e0896022c5625a4c83c5e9c860dbe9e7e
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-10 23:04:20 +0300

    Task18: Draw a plus (Program)

---
+++ Task18_Draw_a_plus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите высоту плюса:");
+        int num = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        for (int i = 0; i < num; i++)
+        {
+            Console.SetCursorPosition(num / 2, i);
+            Console.Write("*");
+            Console.SetCursorPosition(i, num / 2);
+            Console.Write("*");
+        }
+        Console.ReadLine();
+    }
+}
