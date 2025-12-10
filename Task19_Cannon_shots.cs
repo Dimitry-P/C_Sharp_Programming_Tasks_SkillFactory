a32a5238f730e6dea836a0ca1f1e99f324b80f34
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-10 23:06:54 +0300

    Task19: Cannon shots (Program)

---
+++ Task19_Cannon_shots.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите левую границу отрезка");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите правую границу отрезка");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите насколько выстрелила пушка");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n >= a && n <= b)
+        { Console.WriteLine("Попадание"); }
+        else { Console.WriteLine("Промах"); }
+
+        Console.ReadLine();
+    }
+}
