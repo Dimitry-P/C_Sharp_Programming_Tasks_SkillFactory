b7eafaa71cc162db7db01c0f520323dfbf275e93
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-11 20:00:57 +0300

    Task21: Sum, difference, product and quotient of two numbers (Program)

---
+++ Task21_Sum,_difference,_product_and_quotient_of_two_numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int first, second, sum, raz, proiz, chast;
+
+        Console.Write("Введите первое число: ");
+        first = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        second = int.Parse(Console.ReadLine());
+        sum = first + second;
+        raz = first - second;
+        proiz = second * first;
+        chast = first / second;
+        Console.WriteLine($"{first} + {second} = {sum}");
+        Console.WriteLine($"{first} - {second} = {raz}");
+        Console.WriteLine($"{first} * {second} = {proiz}");
+        Console.WriteLine($"{first} / {second} = {chast}");
+
+        Console.ReadKey();
+    }
+}
