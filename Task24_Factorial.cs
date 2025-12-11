3f920bfdff84c74cce9ccb81a904677cdf845787
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-11 20:07:58 +0300

    Task24: Factorial (Program)

---
+++ Task24_Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int i = number - 1;
+        while (i > 0)
+        {
+            number = number * i;
+            i--;
+        }
+        Console.WriteLine(number);
+        Console.ReadKey();
+    }
+}
