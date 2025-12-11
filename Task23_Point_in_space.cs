c2527b93697f5bc1b2c8ac89a7489d144e37dec2
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-11 20:06:01 +0300

    Task23: A point in space (Program)

---
+++ Task23_Point_in_space.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x1 = int.Parse(Console.ReadLine());
+        int y1 = int.Parse(Console.ReadLine());
+        int x2 = int.Parse(Console.ReadLine());
+        int y2 = int.Parse(Console.ReadLine());
+        int Px = int.Parse(Console.ReadLine());
+        int Py = int.Parse(Console.ReadLine());
+
+        if (Px >= x1)
+        {
+            if (Px <= x2)
+            {
+                if (Py >= y1)
+                {
+                    if (Py <= y2)
+                    {
+                        Console.WriteLine("Точка принадлежит прямоугольнику!");
+                    }
+                    else
+                        Console.WriteLine("Промах");
+                }
+                else
+                    Console.WriteLine("Промах");
+            }
+            else
+                Console.WriteLine("Промах");
+        }
+        else
+            Console.WriteLine("Промах");
+        Console.ReadLine();
+    }
+}
