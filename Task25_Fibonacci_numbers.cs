24dbbc5884127f79ecba86e0050a20025e90f051
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-11 20:10:26 +0300

    Task25: Fibonacci numbers (Program)

---
+++ Task25_Fibonacci_numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static int Fibonachi(int number)
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        if (number == 1) return 0;
+        if (number == 2) return 1;
+        if (number == 3) return 1;
+
+        number -= 2;
+
+        while (number != 0)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            number--;
+        }
+        return c;
+    }
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Число фибоначи под номером {number} равно " + Fibonachi(number));
+        Console.ReadLine();
+    }
+}
