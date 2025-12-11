57c4a5d67dab7b02ec8299df2d60477ec5585931
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-11 20:03:48 +0300

    Task22: Multiplication table (Program)

---
+++ Task22_Multiplication_table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int i, j;
+        int k = 0;
+
+        for (i = 1; i <= num; i++)
+        {
+            for (j = 1; j <= num; j++)
+            {
+                int m = 1 + k;
+                Console.Write($"{m * j,-5}");
+            }
+            Console.WriteLine();
+            k = k + 1;
+        }
+        Console.ReadKey();
+    }
+}
