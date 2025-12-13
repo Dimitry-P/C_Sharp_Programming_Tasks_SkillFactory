5b1c2ceecd705ad7d7a91f3a7d41a625631f95bc
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-13 23:07:04 +0300

    Task27: X to the power of Y (Program)

---
+++ Task27_X_to_the_power_of_Y.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int z = x;
+        int y = int.Parse(Console.ReadLine());
+        int i = y;
+
+        while (i > 1)
+        {
+            x *= z;
+            i--;
+        }
+        Console.WriteLine($"{z} в степени {y} равно {x}");
+        Console.ReadLine();
+    }
+}
