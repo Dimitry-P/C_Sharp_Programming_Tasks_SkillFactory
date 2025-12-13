6ae81532d4d8182bbc203389e59d811c396db724
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-13 23:09:25 +0300

    Task28: The constellation Ursa Major (Program)

---
+++ Task28_The_constellation_Ursa_Major.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Созвездие большой медведицы");
+        Console.SetCursorPosition(110, 10);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(30, 20);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(40, 21);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(50, 21);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(110, 23);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(10, 26);
+        Console.WriteLine("*");
+        Console.SetCursorPosition(55, 27);
+        Console.WriteLine("*");
+        Console.ReadKey();
+    }
+}
