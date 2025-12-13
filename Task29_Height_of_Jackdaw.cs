f8ace3d72fc31b42fb56098e5052b7c1182353c5
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-13 23:11:15 +0300

    Task29: Height of Jackdaw (Program)

---
+++ Task29_Height_of_Jackdaw.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите высоту галки:");
+        int height = int.Parse(Console.ReadLine());
+        Console.Clear();
+        for (int i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(i, i);
+            Console.WriteLine("*");
+            Console.SetCursorPosition(height + i, height - i - 1);
+            Console.WriteLine("*");
+        }
+        Console.ReadLine();
+    }
+}
