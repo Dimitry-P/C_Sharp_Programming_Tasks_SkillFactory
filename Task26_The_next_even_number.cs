4babbdffea83ed88b808dbcc0a26d533a77e0e03
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-13 23:03:09 +0300

    Task26: The next even number (Program)

---
+++ Task26_The_next_even_number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int x = int.Parse(Console.ReadLine());
+        int y = x % 2;
+
+        Console.Write("следующее четное число равно: " + (x + (2 - y)));
+        Console.ReadKey();
+    }
+}
