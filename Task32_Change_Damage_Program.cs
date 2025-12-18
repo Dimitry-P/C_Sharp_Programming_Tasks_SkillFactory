dc606e73124ff2c4a07d6a188f047d3e035f03c4
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-18 22:44:35 +0300

    Task32: Change Damage (Weapon, Program)

---
+++ Task32_Change_Damage_Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Weapon
+{
+    public int Damage;
+}
+
+class Program
+{
+    static void ChangeDamage(Weapon w)
+    {
+        w.Damage += 10;
+    }
+    static void Main()
+    {
+        Weapon mainWeapon = new Weapon();
+        mainWeapon.Damage = 10;
+
+        Weapon axe = new Weapon();
+        axe.Damage = 11;
+        Weapon lasergun = new Weapon();
+        lasergun.Damage = 100;
+        ChangeDamage(lasergun);
+
+        Console.WriteLine(lasergun.Damage);
+        Console.ReadLine();
+    }
+}
