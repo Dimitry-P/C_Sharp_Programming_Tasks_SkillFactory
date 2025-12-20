d9814a7ecc740d4e60db2fc4851a9d4e2cd9921d
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-12-20 18:24:07 +0300

    Task33: Two-dimensional array (Program)

---
+++ Task33_Two_dimensional_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // мой вариант
+        int[,] a = new int[4, 4];
+
+        for (int i = 0; i < a.GetLongLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLongLength(1); j++)
+            {
+                if (i % 3 == 0 && j % 3 == 0) a[i, j] = 1;
+                if (i % 3 != 0 && j % 3 != 0) a[i, j] = 1;
+            }
+        }
+
+        for (int i = 0; i < a.GetLongLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLongLength(1); j++)
+            {
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
